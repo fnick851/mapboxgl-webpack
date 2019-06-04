56c63b09c6802dbcd7ebe7680bb32a9e19d4281c
Noah <hanlun.song@gmail.com>
2019-06-03 21:21:53 -0400

    Init

---
+++ README.md
@@ -0,0 +1,29 @@
+# Use mapbox-gl with Webpack
+
+Adapted from: https://docs.mapbox.com/help/tutorials/make-a-heatmap-with-mapbox-gl-js/[https://docs.mapbox.com/help/tutorials/make-a-heatmap-with-mapbox-gl-js/]
+
+## Building and running on localhost
+
+First install dependencies:
+
+```sh
+npm install
+```
+
+To create a production build:
+
+```sh
+npm run build
+```
+
+To develop:
+
+```sh
+npm run dev
+```
+
+## Output
+
+```sh
+dist/bundle.js
+```
---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const config = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.svg$/,
+        use: 'file-loader'
+      },
+      {
+        test: /\.png$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              mimetype: 'image/png'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    hot: true,
+    port: 9999
+  }
+}
+
+module.exports = config;
